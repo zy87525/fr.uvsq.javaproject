fcf8b0359c91951aa7d9631481a8f0f6793ad091
qiriga <qiriga@hotmail.com>
2014-10-19 23:00:05 +0200

    Version 1.0

---
+++ javaproject/src/main/java/tuto/Logic.java
@@ -0,0 +1,59 @@
+package tuto;
+
+import java.io.InputStream;
+import java.util.Iterator;
+
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.Property;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.StmtIterator;
+import com.hp.hpl.jena.util.FileManager;
+
+public class Logic {
+	
+	public void readRDF(RDF rdf){
+		
+		Tuple tuple;
+		// cr閑r un mod鑜e vide
+		Model model = ModelFactory.createDefaultModel();
+		
+		// utiliser le FileManager pour trouver le fichier d'entr閑
+		InputStream inputFileName = FileManager.get().open(rdf.getFileName());
+		if (inputFileName == null) {
+		    throw new IllegalArgumentException("Fichier: " + inputFileName + " non trouv�");
+		}
+		
+		model.read(inputFileName,null);		
+		
+        StmtIterator iter = model.listStatements();
+		while (iter.hasNext()) {
+			// get next statement
+            Statement stmt      = iter.nextStatement();
+            
+			tuple = new Tuple();
+            // get the subject
+			tuple.setRessource(stmt.getSubject()); 
+            tuple.setPredicat(stmt.getPredicate()); // get the predicate
+            tuple.setNode(stmt.getObject());    // get the object
+            
+            rdf.add(tuple);            
+           
+		}
+		
+		for(Tuple t:rdf.tuple){
+
+			 System.out.print(t.getRessource().toString());
+	         System.out.print(" " + t.getPredicat().toString() + " ");
+	         if (t.getNode() instanceof Resource) {
+	             System.out.print(t.getNode().toString()+"zebi");
+	         } else {
+	             // object is a literal
+	             System.out.print(" \"" + t.getNode().toString() + "\"");
+	         }
+	         System.out.println(" .");
+		}
+	}
+}
---
+++ javaproject/src/main/java/tuto/Tuple.java
@@ -0,0 +1,38 @@
+package tuto;
+
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.Resource;
+
+public class Tuple {
+	private Resource Ressource;
+	private Resource Predicat;
+	private RDFNode Node;
+	
+	public Tuple(){}
+	
+	public Tuple(Resource ressource,Resource predicat,RDFNode node){
+		this.Ressource = ressource;
+		this.Predicat = predicat;
+		this.Node = node;
+	}
+	
+	
+	public Resource getRessource() {
+		return Ressource;
+	}
+	public void setRessource(Resource ressource) {
+		this.Ressource = ressource;
+	}
+	public Resource getPredicat() {
+		return Predicat;
+	}
+	public void setPredicat(Resource predicat) {
+		this.Predicat = predicat;
+	}
+	public RDFNode getNode() {
+		return Node;
+	}
+	public void setNode(RDFNode node) {
+		this.Node = node;
+	}
+}
